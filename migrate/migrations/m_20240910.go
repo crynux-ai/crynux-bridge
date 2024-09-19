09a4253a8dd91d12085d2c29475414399983be85
iwehf <henry.lee@crynux.ai>
2024-09-19 11:04:26 +0800

    bug fix: change migration scripts

--- migrate/migrations/m_20240910.go
+++ migrate/migrations/m_20240910.go
@@ -10,18 +10,18 @@ import (
 func M20240910(db *gorm.DB) *gormigrate.Gormigrate {
 	return gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
-			ID: "M20230830",
+			ID: "M20240910",
 			Migrate: func(tx *gorm.DB) error {
 				var err error
 				err = tx.Model(&models.BaseModel{}).
-					Where(&models.BaseModel{Type: models.ModelType_SD_1_5}).
+					Where(&models.BaseModel{Key: "runway/stable-diffusion-v1-5"}).
 					Updates(&models.BaseModel{Key: "crynux-ai/stable-diffusion-v1-5", Link: "https://huggingface.co/crynux-ai/stable-diffusion-v1-5"}).
 					Error
 				if err != nil {
 					return err
 				}
 				err = tx.Model(&models.BaseModel{}).
-					Where(&models.BaseModel{Type: models.ModelType_SD_XL}).
+					Where(&models.BaseModel{Key: "stabilityai/stable-diffusion-xl-base-1.0"}).
 					Updates(&models.BaseModel{Key: "crynux-ai/stable-diffusion-xl-base-1.0", Link: "https://huggingface.co/crynux-ai/stable-diffusion-xl-base-1.0"}).
 					Error
 				if err != nil {
@@ -33,14 +33,14 @@ func M20240910(db *gorm.DB) *gormigrate.Gormigrate {
 			Rollback: func(tx *gorm.DB) error {
 				var err error
 				err = tx.Model(&models.BaseModel{}).
-					Where(&models.BaseModel{Type: models.ModelType_SD_1_5}).
+					Where(&models.BaseModel{Key: "crynux-ai/stable-diffusion-v1-5"}).
 					Updates(&models.BaseModel{Key: "runway/stable-diffusion-v1-5", Link: "https://huggingface.co/runwayml/stable-diffusion-v1-5"}).
 					Error
 				if err != nil {
 					return err
 				}
 				err = tx.Model(&models.BaseModel{}).
-					Where(&models.BaseModel{Type: models.ModelType_SD_XL}).
+					Where(&models.BaseModel{Key: "crynux-ai/stable-diffusion-xl-base-1.0"}).
 					Updates(&models.BaseModel{Key: "stabilityai/stable-diffusion-xl-base-1.0", Link: "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0"}).
 					Error
 				if err != nil {
