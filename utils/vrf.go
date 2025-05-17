aa0161d63350bedb9adc3b0d1002b4ff0ace6f5b
iwehf <henry.lee@crynux.ai>
2025-05-17 19:34:15 +0800

    change vrf validation

--- utils/vrf.go
+++ utils/vrf.go
@@ -4,6 +4,6 @@ import "math/big"
 
 func VrfNeedValidation(vrfNumber []byte) bool {
 	number := big.NewInt(0).SetBytes(vrfNumber)
-	r := big.NewInt(0).Mod(number, big.NewInt(10)).Uint64()
+	r := big.NewInt(0).Mod(number, big.NewInt(100)).Uint64()
 	return r == 0
 }
