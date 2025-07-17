ca07ea864801e19aa5038c9b86ae3f8e128b825c
iwehf <henry.lee@crynux.ai>
2025-07-17 09:36:12 +0800

    revert RelayNode change to adapt v1 api

--- models/relay.go
+++ models/relay.go
@@ -49,9 +49,7 @@ type RelayNode struct {
 	Status        NodeStatus `json:"status" gorm:"index"`
 	GPUName       string     `json:"gpu_name" gorm:"index"`
 	GPUVram       uint64     `json:"gpu_vram" gorm:"index"`
-	QOSScore      float64    `json:"qos_score"`
-	StakingScore  float64    `json:"staking_score"`
-	ProbWeight    float64    `json:"prob_weight"`
+	QOSScore      uint64     `json:"qos_score"`
 	Version       string     `json:"version"`
 	InUseModelIDs []string   `json:"in_use_model_ids"`
 	ModelIDs      []string   `json:"model_ids"`
